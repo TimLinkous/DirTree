29dd43f24d95508b54bf621348da9c6d081f6b79
Tim L <timl@Zipcoders-MacBook-Pro.local>
2024-07-24 14:12:05 -0400

    Finished

--- test/foo/dtg.py
+++ test/foo/dtg.py
@@ -1,10 +1,38 @@
 import os
+import sys
+from pathlib import Path
+
+# def list_files(directory):
+#     for root, dirs, files in os.walk(directory):
+#         for file in files:
+#             print (file)
+
+# list_files('test')
 
 def list_files(directory):
-    for root, dirs, files in os.walk(directory):
+    for root, dirs, files, in os.walk(directory):
+        print(f'Directory: {root}')
         for file in files:
-            print (file)
+            print(f'Files: {file}')
+        # for name in dirs + files:
+        #     if with_paths:
+        #         relative_path = os.path.relpath(os.path.join(root, file), directory)
+        #         print(relative_path)
+        #     else:
+        #         print(name)
 
-list_files('test')
+def list_files_pathlib(directory):
+    pth = Path(directory)
+    for file_path in pth.rglob('*'):
+        if file_path.is_file():
+            print(f"File: {file_path}")
+        elif file_path.is_dir():
+            print(f"Directory: {file_path}")
 
-# def list2_files(directory, with_path = False):
+
+
+if __name__ == "__main__":
+    directory = 'test'
+
+    list_files(directory)
+    list_files_pathlib(directory)
